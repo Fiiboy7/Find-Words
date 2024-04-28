a5eb60caf3fc4762a1e7554d094f2759167f0efc
ghaisan wildan bathsya <160695081+willydans@users.noreply.github.com>
2024-04-28 21:10:13 +0700

    Update Find Words.cpp

--- Find Words.cpp
+++ Find Words.cpp
@@ -96,3 +96,27 @@ bool searchDiagonal(string word) {
 bool searchWord(string word) {
     return searchHorizontal(word) || searchVertical(word) || searchDiagonal(word);
 }
+
+int main() {
+    int N;
+    cout << "Masukkan jumlah kata yang ingin dicari: ";
+    cin >> N;
+    vector<string> words(N);
+    cout << "Masukkan kata-kata yang ingin dicari:" << endl;
+    for (int i = 0; i < N; ++i) {
+        cin >> words[i];
+    }
+
+    
+
+    cout << "Hasil Pencarian:" << endl;
+    for (const string& word : words) {
+        if (searchWord(word)) {
+            cout << word << ": Ada" << endl;
+        } else {
+            cout << word << ": Tidak Ada" << endl;
+        }
+    }
+
+    return 0;
+}
